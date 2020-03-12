11891e25ffeea1ca81d1bdee2f3d93fc1acb0344
Robert Hilla <55510493+RobertHilla@users.noreply.github.com>
2020-03-12 22:14:10 +0100

    Add files via upload

---
+++ src/main/java/io/javaexercise/coronavirustracker/models/LocationStats.java
@@ -0,0 +1,43 @@
+package io.javaexercise.coronavirustracker.models;
+
+public class LocationStats {
+
+    private String state;
+    private String country;
+
+    @Override
+    public String toString() {
+        return "LocationStats{" +
+                "state='" + state + '\'' +
+                ", country='" + country + '\'' +
+                ", latestTotalCases=" + latestTotalCases +
+                '}';
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public int getLatestTotalCases() {
+        return latestTotalCases;
+    }
+
+    public void setLatestTotalCases(int latestTotalCases) {
+        this.latestTotalCases = latestTotalCases;
+    }
+
+    private int latestTotalCases;
+
+}

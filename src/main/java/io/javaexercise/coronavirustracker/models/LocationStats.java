e751b680b75bc567008b1c8f90df4736fcc2dad9
Robert Hilla <55510493+RobertHilla@users.noreply.github.com>
2020-03-15 12:04:24 +0100

    Add files via upload

--- src/main/java/io/javaexercise/coronavirustracker/models/LocationStats.java
+++ src/main/java/io/javaexercise/coronavirustracker/models/LocationStats.java
@@ -4,14 +4,15 @@ public class LocationStats {
 
     private String state;
     private String country;
+    private int latestTotalCases;
+    private int diffFromPrevDay;
 
-    @Override
-    public String toString() {
-        return "LocationStats{" +
-                "state='" + state + '\'' +
-                ", country='" + country + '\'' +
-                ", latestTotalCases=" + latestTotalCases +
-                '}';
+    public int getDiffFromPrevDay() {
+        return diffFromPrevDay;
+    }
+
+    public void setDiffFromPrevDay(int diffFromPrevDay) {
+        this.diffFromPrevDay = diffFromPrevDay;
     }
 
     public String getState() {
@@ -38,6 +39,14 @@ public class LocationStats {
         this.latestTotalCases = latestTotalCases;
     }
 
-    private int latestTotalCases;
+
+    @Override
+    public String toString() {
+        return "LocationStats{" +
+                "state='" + state + '\'' +
+                ", country='" + country + '\'' +
+                ", latestTotalCases=" + latestTotalCases +
+                '}';
+    }
 
 }
